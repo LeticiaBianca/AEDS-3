df654ac24b5864de3adf49b8e97cc382b7d66b09
Leticia Bianca <leticiabiancaoliveira@gmail.com>
2023-02-24 09:00:51 -0300

    Main

--- Airbnb.java
+++ Airbnb.java
@@ -3,18 +3,16 @@
  */
 public class Airbnb {
 
-    protected int id;
+    protected int id; //problema do id não sequencial
     protected String type;
     // lista amenities
     protected int accommodates;
     protected String cacelation;
     protected boolean cleaning; //tem que ter uma string de tamanho fixo
     protected String city;
-    protected String description;
     //data review
     protected String name;
     protected String neigh;
-    protected int number;
     protected int rating;
     
     public Airbnb() {
