cee461a0d262e4529a1fc408d73988310f0b2396
Leticia Bianca <leticiabiancaoliveira@gmail.com>
2023-02-24 08:41:39 -0300

    Main

---
+++ Airbnb.java
@@ -0,0 +1,24 @@
+/**
+ * Airnbnb
+ */
+public class Airbnb {
+
+    protected int id;
+    protected String type;
+    // lista amenities
+    protected int accommodates;
+    protected String cacelation;
+    protected boolean cleaning; //tem que ter uma string de tamanho fixo
+    protected String city;
+    protected String description;
+    //data review
+    protected String name;
+    protected String neigh;
+    protected int number;
+    protected int rating;
+    
+    public Airbnb() {
+
+    }
+    
+}
